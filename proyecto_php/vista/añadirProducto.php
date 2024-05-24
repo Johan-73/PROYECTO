5ebb41d2857b22bf3e06c57f379b9e89904d13ed
Isabella Ramírez <84422617+KepperCommander@users.noreply.github.com>
2024-05-24 00:28:37 -0500

    Update añadirProducto.php

--- proyecto_php/vista/añadirProducto.php
+++ proyecto_php/vista/añadirProducto.php
@@ -3,22 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="../assets/img/cerezossinfondo.png" />
-    <link rel="stylesheet" href="../assets/css/stylesAdmin.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="../assets/css/stylesAñadirProducto.css">
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/62e783ab37.js" crossorigin="anonymous"></script>
     <title>Añadir Productos</title>
 </head>
 <body>
 <?php
     include('headerAdmin.php');
     ?>
-  
+  <div class="container">
     <form action="../controlador/controlador_añadirProducto.php" method="post">
-    <label for="Imagen">IMAGEN:</label>
+    <div class="column-left">
+        <label for="Imagen">IMAGEN:</label>
         <input type="file" name="Imagen" id="Imagen" ><br><br>
-        
+    </div>
+        <div class="column-right">
         <label for="Nombre">NOMBRE:</label>
         <input type="text" id="Nombre" name="Nombre" required><br><br>
         
@@ -39,12 +38,10 @@
         
 
         <input type="submit" value="Guardar">
+        </div>
+</div>
     </form>  
     <?php
-    include('footerAdmin.php');
-    ?>
-</body>
-</html>
     include('footerAdmin.php');
     ?>
 </body>
