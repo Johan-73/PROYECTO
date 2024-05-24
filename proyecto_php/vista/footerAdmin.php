2d13df4f0fde0410078a37ef0dcf869d5dade88c
ManuelaFrancoOquendo <manuelafranco1502@gmail.com>
2024-05-24 02:19:17 -0500

    Commit Final Manuela

--- proyecto_php/vista/footerAdmin.php
+++ proyecto_php/vista/footerAdmin.php
@@ -1,11 +1,12 @@
 <link rel="stylesheet" href="../assets/css/stylesFooter.css">
 
+
 <footer>
     <h3>REDES SOCIALES</h3>
     <a class= "Instagram" href="https://www.instagram.com/cerezos_official/?igsh=MXRrZmsxcDUxdDRtZg%3D%3D" target="_blank">
         <div class="centrado">
             <img class="footer-img" src="../assets/img/logoInstagram.png" alt="logo Instagram">
-            <label>@cerezos_official</label>
+            <label class="redes-sociales">@cerezos_official</label>
         </div>
     
     </a>
@@ -15,7 +16,7 @@
     <a class= "WhatsApp" href="https://wa.me/message/PPJGC2EDA2ZNB1" target="_blank">
         <div class="centrado">
             <img class="footer-img" src="../assets/img/logoWhatsApp.png" alt="logo WhatsApp">
-            <label>+57 3018101603</label>
+            <label class="redes-sociales">+57 3018101603</label>
         </div>
     </a>
    
